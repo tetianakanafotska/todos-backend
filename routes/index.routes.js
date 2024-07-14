2f512cb09478b61e42688be061eb7703c0639d31
tanyakanf <tanyakanf@gmail.com>
2024-07-14 20:15:12 +0200

    rename upload route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,7 +5,7 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
-router.post("/upload", upload.single("image"), (req, res, next) => {
+router.post("/images/upload", upload.single("image"), (req, res, next) => {
   if (!req.file) {
     next(new Error("No image uploaded!"));
     return;
