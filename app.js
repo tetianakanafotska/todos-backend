92d268ec971c8f14a909c9aef63f24334c86c0cb
tanyakanf <tanyakanf@gmail.com>
2024-08-13 19:31:46 +0200

    try secured

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 require("dotenv").config();
 require("./db");
 const express = require("express");
+const app = express();
+require("./config")(app);
 
 const { isAuthenticated } = require("./middleware/jwt.middleware");
-
-const app = express();
-
-require("./config")(app);
 
 const indexRoutes = require("./routes/index.routes");
 app.use("/", indexRoutes);
--- config/index.js
+++ config/index.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const logger = require("morgan");
 const cors = require("cors");
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 
 const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
@@ -11,16 +10,16 @@ const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
 module.exports = (app) => {
   app.set("trust proxy", 1);
 
-  // app.use(
-  //   cors({
-  //     origin: FRONTEND_URL,
-  //     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-  //     allowedHeaders: ["Content-Type", "Authorization"],
-  //     credentials: true,
-  //   })
-  // );
+  app.use(
+    cors({
+      origin: FRONTEND_URL,
+      methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    })
+  );
 
-  app.use(cors());
+  //app.use(cors());
 
   app.use(express.static("public"));
   app.use(logger("dev"));
