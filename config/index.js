6f524844d4ab3e9098d66295537dc0d47059812e
tanyakanf <tanyakanf@gmail.com>
2024-08-13 18:09:12 +0200

    enable cors for all

--- config/index.js
+++ config/index.js
@@ -8,17 +8,15 @@ const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
 module.exports = (app) => {
   app.set("trust proxy", 1);
 
-  app.use(
-    cors({
-      origin: [FRONTEND_URL],
-    })
-  );
-  //app.use(express.static(path.join(__dirname, "public")));
+  // app.use(
+  //   cors({
+  //     origin: [FRONTEND_URL],
+  //   })
+  // );
+  app.use(cors());
   app.use(express.static("public"));
   app.use(logger("dev"));
   app.use(express.json());
 
   app.use(express.urlencoded({ extended: false }));
 };
-
-//see what other stuff need to be added
