ba79c1cc53f7b9d4c6d9520bf8a604b596b5ed94
tanyakanf <tanyakanf@gmail.com>
2024-08-13 13:32:46 +0200

    fix multiple headers sent when signup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,45 +4,53 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User.model");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
   const { email, password, name } = req.body;
+
   if (!email || !password || !name) {
-    res
+    return res
       .status(400)
       .json({ message: "Please provide your email, password, and name" });
-    return;
   }
+
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
   if (!emailRegex.test(email)) {
-    res.status(400).json({ message: "Please provide a valid email address" });
-    return;
+    return res
+      .status(400)
+      .json({ message: "Please provide a valid email address" });
   }
 
   const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
   if (!passwordRegex.test(password)) {
-    res.status(400).json({
+    return res.status(400).json({
       message:
         "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
     });
-    return;
   }
-  User.findOne({ email }).then((foundUser) => {
+
+  try {
+    const foundUser = await User.findOne({ email });
     if (foundUser) {
-      res.status(400).json({ message: "This user already exists" });
-      return;
+      return res.status(400).json({ message: "This user already exists" });
     }
-  });
-  const salt = bcrypt.genSaltSync(10);
-  const hashedPassword = bcrypt.hashSync(password, salt);
-  return User.create({ email, password: hashedPassword, name })
-    .then((createdUser) => {
-      const { email, name, _id } = createdUser;
-      res.status(201).json({ email, name, _id });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ message: "Internal Server Error" });
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const createdUser = await User.create({
+      email,
+      password: hashedPassword,
+      name,
     });
+    const { email: createdEmail, name: createdName, _id } = createdUser;
+
+    return res
+      .status(201)
+      .json({ email: createdEmail, name: createdName, _id });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 router.post("/login", (req, res) => {
