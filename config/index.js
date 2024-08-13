f49c2fd095d333d8e98edf12c7c4d71e5d180e74
tanyakanf <tanyakanf@gmail.com>
2024-08-13 18:58:22 +0200

    allow all request

--- config/index.js
+++ config/index.js
@@ -7,28 +7,22 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 
 const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
-console.log("this is frontedn", FRONTEND_URL);
+console.log("Frontend URL:", FRONTEND_URL);
 
 module.exports = (app) => {
   app.set("trust proxy", 1);
 
-  app.use(
-    cors({
-      origin: FRONTEND_URL,
-      methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-      credentials: true,
-    })
-  );
-  app.options(
-    "*",
-    cors({
-      origin: FRONTEND_URL,
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-      credentials: true,
-    })
-  );
+  // app.use(
+  //   cors({
+  //     origin: FRONTEND_URL,
+  //     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+  //     allowedHeaders: ["Content-Type", "Authorization"],
+  //     credentials: true,
+  //   })
+  // );
+
+  app.use(cors());
+
   app.use(express.static("public"));
   app.use(logger("dev"));
   app.use(express.json());
