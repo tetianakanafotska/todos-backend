5abeb0363366ceb34cc3307c0f199e6c6faf591d
tanyakanf <tanyakanf@gmail.com>
2024-08-13 18:25:57 +0200

    try to fix cors

--- config/index.js
+++ config/index.js
@@ -2,6 +2,7 @@ const logger = require("morgan");
 const cors = require("cors");
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 
 const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
 
@@ -17,6 +18,6 @@ module.exports = (app) => {
   app.use(express.static("public"));
   app.use(logger("dev"));
   app.use(express.json());
-
+  app.use(cookieParser());
   app.use(express.urlencoded({ extended: false }));
 };
