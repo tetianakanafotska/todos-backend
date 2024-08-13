a2e3b6715fc59c223d3c59d77464aec55a151221
tanyakanf <tanyakanf@gmail.com>
2024-08-13 19:34:19 +0200

    back to all

--- config/index.js
+++ config/index.js
@@ -10,16 +10,16 @@ const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
 module.exports = (app) => {
   app.set("trust proxy", 1);
 
-  app.use(
-    cors({
-      origin: FRONTEND_URL,
-      methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-      credentials: true,
-    })
-  );
+  // app.use(
+  //   cors({
+  //     origin: FRONTEND_URL,
+  //     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+  //     allowedHeaders: ["Content-Type", "Authorization"],
+  //     credentials: true,
+  //   })
+  // );
 
-  //app.use(cors());
+  app.use(cors());
 
   app.use(express.static("public"));
   app.use(logger("dev"));
